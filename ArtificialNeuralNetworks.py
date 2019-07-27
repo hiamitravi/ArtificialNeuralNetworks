303e1a5419b2efe2335968f380cf8c2c386babc0
Amit Ravi <hiamit.ravi@gmail.com>
2019-07-27 03:05:39 +0100

    updated ANN

--- ArtificialNeuralNetworks.py
+++ ArtificialNeuralNetworks.py
@@ -37,3 +37,65 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+#Step1 : Randomly initialise the weights to small numbers close to 0. 
+#This is taken care by Dense function, init parameter = 'uniform'
+#Step 2: Input the first observation of dataset in the input layer, each feature in one input node
+#input_dim parameter takes care that.
+#Step 3: Forward propagation: Activated by activation function. The more tha value, the more impact of that neuron.
+#First hidden layer choosen as Rectifier function "relu". 
+#At output layer, we will use "sigmoid" function to provide probability 
+
+
+
+# Adding the input layer and the first hidden layer
+#hidden layer nodes : average of nummber neurons at input layer and output layer (11+1/2=6)
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+#optimizer is gradient discent,
+# Loss function : cross entropy because they are better than MSE because of logarithmic loss
+#metric to evaluate the model 
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
